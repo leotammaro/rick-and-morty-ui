96624dc9030fa7f21b2e88e2a4148bc0863a420b
Mariano Zicavo <marianozicavo@gmail.com>
2023-09-05 13:26:32 -0300

    Add more config

---
+++ src/pages/Character.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function Character() {
+  return (
+    <h1>
+      Character Page
+    </h1>
+  );
+}
+
+export default Character;
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,23 @@
 import { createBrowserRouter } from "react-router-dom"
 import App from "./pages/App";
+import Character from "./pages/Character";
 
 export const router = createBrowserRouter([
     {
       path: "/",
-      element: (
-        <App/>
-      )
+      children: [
+        {
+          path: "",
+          element: (
+            <App/>
+            )
+        },
+        {
+          path: ':id',
+          element: (
+            <Character/>
+          )
+        }
+      ]
     }
   ]);
